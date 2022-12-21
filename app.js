749fae633908aee7f16f45826a0a1198884cc744
Saizalsinha <saizalsinha2003@gmail.com>
2022-12-22 00:51:47 +0530

    Added body parser

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ const request = require("request");
 
 const app = express();
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/",function(req,res){
+  var fName = req.body.firstName;
+  var lName = req.body.lastName;
+  var email = req.body.email;
+});
+
 app.listen(3000, function(){
   console.log("Port is running");
 });
